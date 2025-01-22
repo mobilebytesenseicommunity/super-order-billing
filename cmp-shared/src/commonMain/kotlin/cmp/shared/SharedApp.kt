e295a30b71695d1196bdf72387c5bd74e836f4c8
therajanmaurya <5724482+therajanmaurya@users.noreply.github.com>
2025-01-22 17:54:32 +0000

    chore: Sync directories and files from upstream

--- cmp-shared/src/commonMain/kotlin/cmp/shared/SharedApp.kt
+++ cmp-shared/src/commonMain/kotlin/cmp/shared/SharedApp.kt
@@ -1,11 +1,11 @@
 /*
- * Copyright 2024 Android Byte Sensei
+ * Copyright 2024 Mifos Initiative
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  *
- * See See https://github.com/androidbytesensei/super-order-billing/blob/dev/LICENSE
+ * See See https://github.com/openMF/kmp-project-template/blob/main/LICENSE
  */
 package cmp.shared
 
