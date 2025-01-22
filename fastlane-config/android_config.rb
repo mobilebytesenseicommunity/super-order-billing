87cfbd799786dc1af5e40893152a2b41bfc02572
Sk Niyaj Ali <niyaj639@gmail.com>
2025-01-23 01:02:44 +0530

    chore: Configure Firebase App Distribution
    This commit updates various project configurations and dependencies:
    
    - Updates Firebase tester group to use a secret variable for better security.
    - Updates Android keystore configurations with new values for password, alias, and key password.
    - Updates Firebase app IDs for production and demo environments.
    - Updates Android and desktop package names and namespaces.
    - Updates google-services.json with the new package name for Android.
    - Potentially updates or adds new dependencies in the libraries section of gradle/libs.versions.toml.
    - Changes release keystore.

--- fastlane-config/android_config.rb
+++ fastlane-config/android_config.rb
@@ -2,16 +2,16 @@ module FastlaneConfig
   module AndroidConfig
     STORE_CONFIG = {
       default_store_file: "release_keystore.keystore",
-      default_store_password: "Wizard@123",
-      default_key_alias: "kmp-project-template",
-      default_key_password: "Wizard@123"
+      default_store_password: "Sensei@2k25",
+      default_key_alias: "sensei-projects",
+      default_key_password: "Sensei@2k25"
     }
 
     FIREBASE_CONFIG = {
-      firebase_prod_app_id: "1:728434912738:android:d853a78f14af0c381a1dbb",
-      firebase_demo_app_id: "1:728434912738:android:8392hjksak9032skja",
+      firebase_prod_app_id: "1:749347496134:android:c207d3a8586f97db63b7c7",
+      firebase_demo_app_id: "1:749347496134:android:080da6859ca5967d63b7c7",
       firebase_service_creds_file: "secrets/firebaseAppDistributionServiceCredentialsFile.json",
-      firebase_groups: "kmp-project-template-testers"
+      firebase_groups: "sensei-app-testers"
     }
 
     BUILD_PATHS = {
